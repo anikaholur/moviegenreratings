b795b85aa0805feedf817da375fecb23a134cbb1
anikaholur <123847416+anikaholur@users.noreply.github.com>
2023-07-28 16:22:14 -0400

    Update movies.py (unsafe)

--- movies.py
+++ movies.py
@@ -13,10 +13,7 @@ warnings.filterwarnings("ignore")
 # Uses st.cache_resource to only run once.
 @st.cache_resource
 def init_connection():
-    return pymongo.MongoClient(**st.secrets["mongo"])
-#@st.experimental_singleton(suppress_st_warning=True)
-#def init_connection():
-    #return pymongo.MongoClient("mongodb+srv://anikaholur8:Sarcasm_1234@mymcluster.qrqm7sw.mongodb.net/?retryWrites=true&w=majority")
+    return pymongo.MongoClient("mongodb+srv://anikaholur8:Sarcasm_1234@mymcluster.qrqm7sw.mongodb.net/?retryWrites=true&w=majority")
 
 
 client = init_connection()
